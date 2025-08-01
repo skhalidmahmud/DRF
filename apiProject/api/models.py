9cb5ebd42e315de821a2a3a33f57fd315b911a72
Khalid Mahmud <skhalidmahmud1@gmail.com>
2025-08-01 11:22:21 +0600

    Auto generated roll

--- apiProject/api/models.py
+++ apiProject/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class studentModel(models.Model):
+    username = models.CharField(max_length=100, null=True)
     studentRoll = models.IntegerField(null=True)
     Name = models.CharField(max_length=100, null=True)
     Age = models.IntegerField(null=True)
--- apiProject/api/serializers.py
+++ apiProject/api/serializers.py
@@ -7,7 +7,13 @@ class studentSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
+        last_man = models.studentModel.objects.order_by('-studentRoll').first()
+        last_roll = last_man.studentRoll
+        if last_roll:
+            last_roll = last_roll+1
+        else:
+            last_roll = 1
         data = models.studentModel.objects.create(**validated_data)
-        data.studentRoll = 50
+        data.studentRoll = last_roll
         data.save()
         return data
